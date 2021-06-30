5843005540e2a480f5cf139f6c8a353e7052626e
wtface <nkhpark@hotmail.com>
2021-06-30 14:28:06 +0800

    chore: migrations

--- migrations/20210630055155-products-table.js
+++ migrations/20210630055155-products-table.js
@@ -19,7 +19,7 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20210623014725-books-table-up.sql');
+  var filePath = path.join(__dirname, 'sqls', '20210630055155-products-table-up.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
@@ -34,7 +34,7 @@ exports.up = function(db) {
 };
 
 exports.down = function(db) {
-  var filePath = path.join(__dirname, 'sqls', '20210623014725-books-table-down.sql');
+  var filePath = path.join(__dirname, 'sqls', '20210630055155-products-table-down.sql');
   return new Promise( function( resolve, reject ) {
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
       if (err) return reject(err);
---
+++ migrations/sqls/20210630055052-users-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    password_digest VARCHAR NOT NULL
+);
